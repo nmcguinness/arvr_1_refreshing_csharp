92510308e2b90ada55072ee751c641488b283aa4
niall mcguinness <niall.mcguinness@dkit.ie>
2021-02-08 15:56:49 +0000

    Add project files.

---
+++ arvr_1_refreshing_csharp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+/// <summary>
+/// Demonstrates core C# concepts useful when programming in Unity
+/// </summary>
+namespace ARVR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.Start();
+        }
+
+        private void Start()
+        {
+            DemoEnum();
+            DemoStruct();
+            DemoInterface();
+            DemoAbstract();
+            DemoGeneric();
+            DemoNamespace();
+            DemoPredicate();
+            DemoFunc();
+            DemoDelegateAction();
+        }
+
+        private void DemoEnum()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoStruct()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoInterface()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoAbstract()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoGeneric()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoNamespace()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoPredicate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoFunc()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DemoDelegateAction()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
